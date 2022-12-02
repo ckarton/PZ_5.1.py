d437301c68919bf081ed2dcf29cc4f3b904e7b76
ckarton <106053147+ckarton@users.noreply.github.com>
2022-12-02 22:32:36 +0300

    add file

---
+++ PZ_6/PZ_6_1.py
@@ -0,0 +1,21 @@
+try:
+    a = []
+
+    N = int(input("Введите размер списка N: "))
+
+    for i in range(N):
+        a.append(int(input("Введите элемент массива: ")))
+
+    K = int(input("Введите K>1: "))
+    L = int(input("Введите N>L>=k: "))
+
+    sum = 0
+    for i in range(0, K - 1):
+        sum += a[i]
+    for i in range(L, N):
+        sum += a[i]
+
+    c = sum / (())
+    print(c)
+except:
+    print("Перезагрузите или попробуте позже")
