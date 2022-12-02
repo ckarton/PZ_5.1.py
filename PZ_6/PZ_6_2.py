65c28a1b15642d5fbf42163be318fb8d013b17a8
ckarton <106053147+ckarton@users.noreply.github.com>
2022-12-02 22:33:38 +0300

    add file

---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,13 @@
+A = []
+B = []
+simv = int(input("Введите колличество символов в символов в массивах: "))
+N=0
+while N<simv:
+  A.append(int(input("Введите элемент массива a: ")))
+  B.append(int(input("Введите элемент массива b: ")))
+  N += 1
+A,B = B,A
+print("Массив а")
+print(A)
+print("Массив b")
+print(B)
