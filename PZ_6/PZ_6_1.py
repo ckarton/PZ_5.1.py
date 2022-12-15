5b0552888687b89c5ab07dd8eabaf78e672d5049
ckarton <106053147+ckarton@users.noreply.github.com>
2022-12-15 20:26:43 +0300

    Update PZ_6_1.py

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,21 +1,28 @@
-try:
-    a = []
-
-    N = int(input("Введите размер списка N: "))
-
-    for i in range(N):
-        a.append(int(input("Введите элемент массива: ")))
-
-    K = int(input("Введите K>1: "))
-    L = int(input("Введите N>L>=k: "))
-
-    sum = 0
-    for i in range(0, K - 1):
-        sum += a[i]
-    for i in range(L, N):
-        sum += a[i]
-
-    c = sum / len(sum)
-    print(c)
-except:
-    print("Перезагрузите или попробуте позже")
+import random
+import statistics as stat
+N = random.randint(10,20)
+L = random.randrange(1,N)
+K = random.randint(1,L)
+#N = 10
+print("N = ", N)
+print("L = ", L)
+print("K = ", K)
+A = [random.randrange(1,20) for i in range(N)]
+print("Initial:")
+print(A)
+s = 0
+c = 0
+for i in range(0,N):
+    if i not in range(K,L+1):
+        c += 1
+        s += A[i]
+        print(i,":",A[i])
+print("Sum = ",s)
+print("Count = ",c)
+print("Average = ", s/c)
+A1 = [j for i, j in enumerate(A) if i not in range(K,L+1)]
+print()
+print(A1)
+print("Sum = ",sum(A1))
+print("Count = ", len(A1))
+print("Average = ", stat.mean(A1))
