7690549bc9d386363dfcd2a014a322ce0d29e9e2
ckarton <106053147+ckarton@users.noreply.github.com>
2022-12-15 20:10:39 +0300

    Update PZ_4_1.py

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,14 +1,14 @@
 import math
 try:
-x = float(input('Введите -1<x<1: ')) #Ввод x
-n = int(input('Введите N>0: ')) #Ввод N
-a = (-1)**n
-b = x**(2*n+1)
-c = 2*n+1
-vsp = a*b/c
-while n>0:
+  x = float(input('Введите -1<x<1: ')) #Ввод x
+  n = int(input('Введите N>0: ')) #Ввод N
+  a = (-1)**n
+  b = x**(2*n+1)
+  c = 2*n+1
+  vsp = a*b/c
+  while n>0:
     resul = vsp + vsp
     n = n-1
-print(math.atan(resul), "радиан")
+  print(math.atan(resul), "радиан")
 except :
 print('вы ввели неверное число')
