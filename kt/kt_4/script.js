f14fd464735b10c4dc16b1085445a977feaf23f6
ckarton <106053147+ckarton@users.noreply.github.com>
2023-07-01 10:58:57 +0300

    Update script.js

--- kt/kt_4/script.js
+++ kt/kt_4/script.js
@@ -1,32 +1,49 @@
 // Вариант 22: Арифметические действия над числами пронумерованы следующим образом: 1 — сложение, 2 — вычитание, 3 — умножение, 4 — деление. 
 // Дан номер действия N (целое число в диапазоне 1-4) и числа A и B (В не равно 0). Выполнить над числами указанное действие и вывести результат.
-function performOperation(N, A, B) {
-    var result;
+<!DOCTYPE html>
+<html>
 
-    switch (N) {
-        case 1:
-            result = A + B;
-            break;
-        case 2:
-            result = A - B;
-            break;
-        case 3:
-            result = A * B;
-            break;
-        case 4:
-            result = A / B;
-            break;
-        default:
-            console.log("Неправильный номер действия.");
-            return;
+<head>
+    <title>Форма взаимодействия с пользователем</title>
+    <script>
+    let deis;
+    function sum(A, B) {
+        let a = Number(document.getElementById("fst").value); // Первое число
+        let b = Number(document.getElementById("snd").value); // Второе число
+        let result;
+            switch (deis) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            }
+        alert(result)
     }
+    </script>
+</head>
 
-    console.log("Результат: " + result);
-}
+<body>
+    <form>
+        <label for="name">Первое число: </label>
+        <input type="text" id="fst" name="fst" required><br>
 
-// Пример
-var N = 4; // Номер действия 
-var A = 1; // Первое число
-var B = 9; // Второе число
+        <label for="name">Второе число: </label>
+        <input type="text" id="snd" name="snd" required><br>
 
-performOperation(N, A, B);
+        <button type="button" onclick="deis = '+'">Сложить</button>
+        <button type="button" onclick="deis = '-'">Вычесть</button>
+        <button type="button" onclick="deis = '*'">Умножить</button>
+        <button type="button" onclick="deis = '/'">Разделить</button><br>
+        <button type="button" onclick="sum();">Посчитать</button>
+    </form>
+</body>
+
+</html>
