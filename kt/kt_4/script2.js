399de0d1b84bf4c6b66e3af168244a0ef56feba9
ckarton <106053147+ckarton@users.noreply.github.com>
2023-07-01 11:00:57 +0300

    Update script2.js

--- kt/kt_4/script2.js
+++ kt/kt_4/script2.js
@@ -1,9 +1,9 @@
 // Вариант 22: Составить программу вычисления функции  y=n∑i=1 (x^2i + 2x^3 )
 function calculateY(n, x) {
-    var sum = 0;
+    let sum = 0;
 
-    for (var i = 1; i <= n; i++) {
-        var term = Math.pow(x, 2 * i) + 2 * Math.pow(x, 3);
+    for (let i = 1; i <= n; i++) {
+        let term = Math.pow(x, 2 * i) + 2 * Math.pow(x, 3);
         sum += term;
     }
 
@@ -11,8 +11,8 @@ function calculateY(n, x) {
 }
 
 // Пример использования функции
-var n = 3; // Количество итераций
-var x = 2; // Значение переменной
+let n = 3; // Количество итераций
+let x = 2; // Значение переменной
 
-var result = calculateY(n, x);
+let result = calculateY(n, x);
 console.log("Результат вычисления функции Y: " + result);
